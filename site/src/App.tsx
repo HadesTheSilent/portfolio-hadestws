fff8583a5f9c85e506a33cc4d39abd80f71521c8
Jullian Salles <jsallesjf@hotmail.com>
2025-10-23 10:19:05 -0300

    fix: update vercel analytics implementation

--- site/src/App.tsx
+++ site/src/App.tsx
@@ -1,11 +1,9 @@
-import { Analytics } from '@vercel/analytics/react'
+import { inject } from '@vercel/analytics'
 import Home from './pages/Home'
 
+// Initialize analytics
+inject()
+
 export default function App() {
-  return (
-    <>
-      <Home />
-      <Analytics />
-    </>
-  )
+  return <Home />
 }
